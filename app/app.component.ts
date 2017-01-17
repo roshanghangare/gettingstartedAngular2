b1bcf2d049e78cd974a8c400466521f551874db9
Roshan <roshanghangare@gmail.com>
2017-01-17 22:34:36 +0530

    class created

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,3 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
-})
-export class AppComponent { }
+export class AppComponent{
+    
+}
