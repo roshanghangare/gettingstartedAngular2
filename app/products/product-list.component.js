38125acb857b28b9227be706ad2267d92923e64e
Roshan <roshanghangare@gmail.com>
2017-01-20 23:12:01 +0530

    wip

--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -6,17 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
-    function AppComponent() {
-        this.pageTitle = 'Acme Product Management';
+var ProductListComponent = (function () {
+    function ProductListComponent() {
+        this.pageTitle = 'Product List';
     }
-    return AppComponent;
+    return ProductListComponent;
 }());
-AppComponent = __decorate([
+ProductListComponent = __decorate([
     core_1.Component({
-        selector: 'pm-app',
-        template: "\n     <div><h1>{{pageTitle}}</h1></div>\n     <pm-products></pm-products>\n     "
+        selector: 'pm-products',
+        templateUrl: 'app/products/product-list.component.html'
     })
-], AppComponent);
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+], ProductListComponent);
+exports.ProductListComponent = ProductListComponent;
+//# sourceMappingURL=product-list.component.js.map
