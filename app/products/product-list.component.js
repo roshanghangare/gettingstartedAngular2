c5db8842c00836d561fb05f1ffa16d1112c38bfe
Roshan Ghangare <Roshan.Ghangare@Xoriant.Com>
2017-02-01 20:08:32 +0530

    Show and hide images on click of button

--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -9,6 +9,9 @@ var core_1 = require("@angular/core");
 var ProductListComponent = (function () {
     function ProductListComponent() {
         this.pageTitle = 'Product List';
+        this.imageWidth = 50;
+        this.imageMargin = 2;
+        this.showImage = false;
         this.products = [
             {
                 "productId": 1,
@@ -62,6 +65,9 @@ var ProductListComponent = (function () {
             }
         ];
     }
+    ProductListComponent.prototype.toggleImage = function () {
+        this.showImage = !this.showImage;
+    };
     return ProductListComponent;
 }());
 ProductListComponent = __decorate([
--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import {Component} from '@angular/core';
 })
 export class ProductListComponent{
     pageTitle:string='Product List';
+    imageWidth:number=50;
+    imageMargin:number=2; 
+    showImage:boolean=false;
     products:any []=[
     {
         "productId": 1,
@@ -57,5 +60,8 @@ export class ProductListComponent{
         "starRating": 4.6,
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
-]
+];
+toggleImage():void{
+    this.showImage=!this.showImage;
+}
 }
